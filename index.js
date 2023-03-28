ce06feba88eb7fc52e5be2cbcdd6f4a789f8d1d5
pashafaiz <mfaizpasha104@gmail.com>
2023-03-28 21:23:52 +0530

    update5

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const { route } = require('./router/WishlistRouter');
 const dotenv = require('dotenv').config();
+const path=require('path')
+const multer =require('multer')
 const port = process.env.PORT || 5000;
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
 const cors = require('cors');
 
 const ConnectDB = require('./config/db');
+const fileUpload = require('./utils/fileUpload');
 ConnectDB()
 
 app.use(express.urlencoded({ extended: false }));
@@ -19,8 +23,16 @@ app.use('/api/allCategories',require('./dataRouter'))
  app.use('/api/address', require('./router/AddreshRouter'));
 app.use('/api/wishlish',require('./router/WishlistRouter'));
  app.use('/api/shopeen', require('./router/SignupRouter'));
+ 
 
 
 app.listen(port, () => {
     console.log(`port is colled ${port}`)
 });
+
+
+app.post("/create",
+fileUpload("profile").array("photo",5),
+(req,res)=>{
+    console.log("=====>",req.body);
+    res.json({  message: "image added...", image: `http://localhost:8000/uploads/images/profile/${req.files[0].filename}` })})
